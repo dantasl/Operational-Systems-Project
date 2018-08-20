5d97260bc19b8b04c46a72721bc19de58b6100ac
Lucas Gomes Dantas <lucas_gomesdantas@hotmail.com>
2018-08-20 19:40:37 -0300

    Doc'ing this part

--- big-brother/src/status.cpp
+++ big-brother/src/status.cpp
@@ -5,13 +5,23 @@
  * @date    August 20th - 2018
  */
 
-#include <iostream>
-#include <fstream>
-#include <stdlib.h>
-#include <string>
-#include <iterator>
-#include <vector>
+#include <iostream> // std::cout, std::endl, etc
+#include <fstream> // ifstream and file.open()
+#include <stdlib.h> // exit() and EXIT_FAILURE
+#include <string> // String manipulation
+#include <iterator> // Iterator manipulation
+#include <vector> // std::vector
 
+/**
+ * @brief   This function will receive a PID and a list of arguments containing what the user
+ *          wants to know about the process by its status. It will read the /proc/pid/status
+ *          and will add to a vector only the lines that match what the user wants.
+ *
+ * @param[in]   pid         The process ID to read the status
+ * @param[in]   arguments   Reference to vector containing the items that the user wants to know
+ *                          about the process.
+ * @return      The data found inside status.
+ */
 std::vector<std::string> get_info (const char* pid, const std::vector<std::string> &arguments)
 {
     std::ifstream status;
@@ -33,6 +43,11 @@ std::vector<std::string> get_info (const char* pid, const std::vector<std::strin
     return info;
 }
 
+/**
+ * @brief   This function will receive a vector containing the information to be printed on screen.
+ *
+ * @param[in]   result      Vector containing all information to print
+ */
 void print_result (std::vector<std::string> result)
 {
     for ( auto it(result.begin()); it != result.end(); ++it )
@@ -41,12 +56,14 @@ void print_result (std::vector<std::string> result)
 
 int main (int const argc, char const **argv)
 {
+    // It must have exactly two arguments: the name of the program and the PID
     if (argc != 2)
     {
         std::cout << "Invalid arguments." << std::endl;
         exit (EXIT_FAILURE);
     }
 
+    // This can be changed to whatever arguments you want to retrieve from /proc/pid/status
     std::vector<std::string> arguments = { "Name", "State", "voluntary_ctxt_switches", "nonvoluntary_ctxt_switches" };
 
     print_result(get_info (argv[1], arguments));
