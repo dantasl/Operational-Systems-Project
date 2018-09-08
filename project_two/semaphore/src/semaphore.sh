7f72be22f625d08fa7777301d446237067977a4e
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2018-09-08 15:40:58 -0300

    Refactoring code for semaphore.sh

--- project_two/semaphore/src/semaphore.sh
+++ project_two/semaphore/src/semaphore.sh
@@ -18,28 +18,28 @@ PANIC_BUTTON=115
 
 #######################################################################
 
-# Section 1 - First configuration that the GPIO needs
+# Section 1 - Bootstraping GPIO and providing helper to LED's output
 #######################################################################
 
+# Export the button and set direction to "in"
 initButton()
 {
 	echo $1 > $EXPORT_DIR
-	echo $1 exported
-
 	echo in > $GPIO_DIR$1/direction
-	cat $GPIO_DIR$1/direction
 }
 
+# Export the pin and set LED output to 0
 initLed()
 {
 	echo $1 > $EXPORT_DIR
-	echo $1 exported
-
 	echo out > $GPIO_DIR$1/direction
-	cat $GPIO_DIR$1/direction
-
 	echo 0 > $GPIO_DIR$1/value
-	cat $GPIO_DIR$1/value
+}
+
+# Change the state of a given LED - e.g. Put RED_LED to 1 (turn on)
+updateLed()
+{
+	echo $2 > $GPIO_DIR$1/value
 }
 
 initLed $LED_RED
@@ -47,42 +47,31 @@ initLed $LED_YELLOW
 initLed $LED_GREEN
 initButton $PANIC_BUTTON
 
+printf "====================================================="
+printf "RED TO $LED_RED\nYELLOW TO $LED_YELLOW\nGREEN TO $LED_GREEN\nBUTTON TO $PANIC_BUTTON\n"
+printf "====================================================="
+
 #######################################################################
 
-# Function that treats the ending of this script
-finish()
-{
-    printf "\nBig Brother now rests at peace.\n"
-    kill -s SIGTERM $!
-    exit 0
-}
+# Section 2 - The main loop, where the magic happens
+#######################################################################
 
-# Change the state of a given LED - e.g. Put RED_LED to 1 (turn on)
-updateLed()
-{
-	echo $2 > $GPIO_DIR$1/value
-	echo $1 to $2
-}
+printf "Big Brother is now watching your processes every 2 seconds.\n\n"
 
-# Here we can check which process is consuming more memory and get its PID
-getTopMemoryUsagePID()
-{
-    BAD_PID=`ps -eo pid --no-headers --sort=-%mem | head -1`
-    echo $BAD_PID
-}
+while [ 1 ]; do
+    printf "\n===============================================\n"
+    PERCENTAGE=$(checkResourceStatus)
+    printf "Maximum memory usage goes by $PERCENTAGE."
+    updateState $PERCENTAGE
+    printf "\n===============================================\n"
+    sleep 2
+done
 
 # Here we will check how much % the resource is being used
 checkResourceStatus()
 {
     PERCENTAGE=`ps -eo %mem --no-headers --sort=-%mem | head -1`
     echo $PERCENTAGE
-}
-
-# Here we will check if the panic button was pressed
-checkPanicButtonPressed()
-{
-    PRESSED=`cat $GPIO_DIR$PANIC_BUTTON/value`
-    echo $PRESSED
 }
 
 # Updates how the system will behave depending on the percentage
@@ -105,27 +94,25 @@ updateState()
     fi
 }
 
+#######################################################################
+
+# Section 3 - Handling the panic mode
+#######################################################################
+
 # Here the LEDs go berserk and we must check if user pressed the button
 initiatePanic()
 {
     LOOP_KILL=1
     while [ $LOOP_KILL -eq 1 ]; do
         # Leds are now blinking
-        updateLed $LED_GREEN 1
-        updateLed $LED_RED 1
-        updateLed $LED_YELLOW 1
-        sleep 0.5
-        updateLed $LED_GREEN 0
-        updateLed $LED_RED 0
-        updateLed $LED_YELLOW 0
-        sleep 0.5
+        blinkLeds
         # Check if button is being pressed
         PRESSED=$(checkPanicButtonPressed)
         if [ $PRESSED -eq 1 ]; then
             # Kill process
             PID=$(getTopMemoryUsagePID)
             kill -9 $PID
-            # LEDs are turnd off for 3 seconds
+            # LEDs are turned off for 3 seconds
             updateLed $LED_GREEN 0
             updateLed $LED_RED 0
             updateLed $LED_YELLOW 0
@@ -136,15 +123,46 @@ initiatePanic()
     done
 }
 
+# Here the LEDs will start to blink
+blinkLeds()
+{
+    updateLed $LED_GREEN 1
+    updateLed $LED_RED 1
+    updateLed $LED_YELLOW 1
+    sleep 0.5
+    updateLed $LED_GREEN 0
+    updateLed $LED_RED 0
+    updateLed $LED_YELLOW 0
+    sleep 0.5
+}
+
+# Here we will check if the panic button was pressed
+checkPanicButtonPressed()
+{
+    PRESSED=`cat $GPIO_DIR$PANIC_BUTTON/value`
+    echo $PRESSED
+}
+
+# Here we can check which process is consuming more memory and get its PID
+getTopMemoryUsagePID()
+{
+    BAD_PID=`ps -eo pid --no-headers --sort=-%mem | head -1`
+    echo $BAD_PID
+}
+
+#######################################################################
+
+# Section 4 - Handling SIGTERM and SIGINT
+#######################################################################
+
+# Function that treats the ending of this script
+finish()
+{
+    printf "\nBig Brother now rests at peace.\n"
+    kill -s SIGTERM $!
+    exit 0
+}
+
 trap finish SIGINT SIGTERM
 
-printf "Big Brother is now watching your processes every 2 seconds.\n\n"
-
-while [ 1 ]; do
-    printf "\n===============================================\n"
-    PERCENTAGE=$(checkResourceStatus)
-    printf "Maximum memory usage goes by $PERCENTAGE."
-    updateState $PERCENTAGE
-    printf "\n===============================================\n"
-    sleep 2
-done
+#######################################################################
