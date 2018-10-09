cdaf42d7224f602487066162348428a2f484c23c
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2018-10-09 09:31:31 -0300

    Client side okay.

--- banjo/src/client.py
+++ banjo/src/client.py
@@ -5,7 +5,11 @@ PORT = 65432           # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    with open("instructions.txt", "r") as f:
+        instruction = f.readline().strip()
+        while instruction == "":
+            instruction = f.readline().strip()
+    s.sendall(bytes(instruction, encoding='utf-8'))
     data = s.recv(1024)
 
-print('Received', repr(data))
+print('Server response:', repr(data))
